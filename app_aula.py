6d95ed1586e0f57d6f9a1726bacedc4cc24d8532
Gustmack <42626322+Gustmack@users.noreply.github.com>
2024-04-22 15:23:20 -0300

    Update app_aula.py

--- app_aula.py
+++ app_aula.py
@@ -91,11 +91,10 @@ with tab1:
 with tab2:
     if database == 'CSV' and file and Xtest is not None:
         st.header("Análise Detalhada")
-        analysis_tab1, analysis_tab2, analysis_tab3, analysis_tab4 = st.tabs(
-            ["Gráfico de Dispersão Plotly 1", "Gráfico de Dispersão Plotly 2", "Gráfico Altair", "Box Plot Seaborn"])
+        analysis_tab1, analysis_tab2, analysis_tab3, analysis_tab4 = st.tabs(["Gráfico de Dispersão Plotly 1", "Gráfico de Dispersão Plotly 2", "Gráfico Altair", "Box Plot Seaborn"])
 
-        with analysis_tab1:
-            st.subheader("Relação entre Recency e Income")
+      with analysis_tab1:
+          st.subheader("Relação entre Recency e Income")
             fig = px.scatter(
                 Xtest,
                 x="Recency",
@@ -105,8 +104,8 @@ with tab2:
             )
             st.plotly_chart(fig, use_container_width=True)
 
-        with analysis_tab2:
-            st.subheader("Dispersão de Recency, Income e Total Purchases")
+      with analysis_tab2:
+          st.subheader("Dispersão de Recency, Income e Total Purchases")
             fig = px.scatter(
                 Xtest,
                 x="Recency",
